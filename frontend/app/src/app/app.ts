9ff59d1572802e28d20b33a540422c7a0ea4d98e
Vikagoldie <viktoria19041409@gmail.com>
2026-06-26 10:41:27 +0200

    REST API

--- frontend/app/src/app/app.ts
+++ frontend/app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, User],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'app';
