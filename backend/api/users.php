8df2a039d60055b27216056e830296036221605f
Vikagoldie <viktoria19041409@gmail.com>
2026-06-26 10:41:27 +0200

    REST API

--- backend/api/users.php
+++ backend/api/users.php
@@ -1,10 +1,18 @@
 <?php
 
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
 
 echo json_encode([
     [
         "id" => 1,
-        "name" => "Max"
+        "username" => "Max",
+        "firstname" => "Topr",
+        "lastname" => "Max",
+        "email" => "Max",
+        "pw" => "Max",
+        "created" => "Max",
     ]
 ]);
