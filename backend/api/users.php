f9c4103c308bc1dbccade96d90ef773251b489cb
Vikagoldie <viktoria19041409@gmail.com>
2026-07-08 19:30:20 +0200

    Database connection

--- backend/api/users.php
+++ backend/api/users.php
@@ -5,14 +5,30 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
-echo json_encode([
-    [
-        "id" => 1,
-        "username" => "Max",
-        "firstname" => "Topr",
-        "lastname" => "Max",
-        "email" => "Max",
-        "pw" => "Max",
-        "created" => "Max",
-    ]
-]);
+use ppa\Config\Database;
+use ppa\Model\UserModel;
+
+require_once '../config/Database.php';
+require_once '../models/UserModel.php';
+
+
+try {
+
+    $database = new Database();
+    $pdo = $database->linkDB();
+
+    $userModel = new UserModel($pdo);
+
+    $users = $userModel->getAll();
+
+    echo json_encode($users);
+
+} catch (Exception $e) {
+
+    http_response_code(500);
+
+    echo json_encode([
+        "success" => false,
+        "message" => $e->getMessage()
+    ]);
+}
---
+++ backend/config/database.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace ppa\Config;
+use PDO;
+use PDOException;
+
+class Database {
+    
+   
+    private $dbName = "pbbfa24akr_av"; //Datenbankname
+    private $linkName = "mysql.pb.bib.de"; //Datenbank-Server
+    private $user = "pbbfa24akr"; //Benutzername
+    private $pw = "x5uMyFQSv87E"; //Passwort
+    
+    /**
+     * Stellt eine Verbindung zur Datenbank herss
+     * 
+     * @return \PDO Gibt eine Datenbankverbindung zurueck
+     */
+    public function linkDB() {
+        try {
+            $pdo = new \PDO("mysql:dbname=$this->dbName;host=$this->linkName"
+                , $this->user
+                , $this->pw
+                , array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
+            return $pdo;
+        } catch (\PDOException $e) {
+            echo json_encode([
+                "success" => false,
+                "message" => $e->getMessage()
+            ]);
+        
+            exit;
+        }
+    }
+
+}
+
+   
+
+
