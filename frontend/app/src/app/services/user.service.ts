02f82710c6d9f9758c5d6b46bcfa81090883f472
Vikagoldie <viktoria19041409@gmail.com>
2026-07-17 11:59:03 +0200

    Routers

--- frontend/app/src/app/services/user.service.ts
+++ frontend/app/src/app/services/user.service.ts
@@ -8,6 +8,6 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUsers() {
-    return this.http.get('http://localhost:8000/users.php');
+    return this.http.get('http://localhost:8000/RestAPI.php');
   }
 }
