96a78d893f5aef9dfc0d092f69567a4320866a10
Vikagoldie <viktoria19041409@gmail.com>
2026-07-17 11:59:03 +0200

    Routers

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,3 +1,61 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayout } from './layouts/layout/main-layout';
+
+import { Home } from './features/home/home';
+import { Products } from './features/products/products';
+import { ProductDetail } from './features/product-detail/product-detail';
+import { Cart } from './features/cart/cart';
+import { Login } from './features/login/login';
+import { Register } from './features/register/register';
+import { Profile } from './features/profile/profile';
+import { Checkout } from './features/checkout/checkout';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+
+      {
+        path: 'products',
+        component: Products,
+      },
+
+      {
+        path: 'products/:id',
+        component: ProductDetail,
+      },
+
+      {
+        path: 'cart',
+        component: Cart,
+      },
+
+      {
+        path: 'login',
+        component: Login,
+      },
+
+      {
+        path: 'register',
+        component: Register,
+      },
+
+      {
+        path: 'profile',
+        component: Profile,
+      },
+
+      {
+        path: 'checkout',
+        component: Checkout,
+      },
+    ],
+  },
+];
--- frontend/app/src/app/app.ts
+++ frontend/app/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { User } from './components/user/user';
+import { MainLayout } from './layouts/layout/main-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, User],
+  imports: [RouterOutlet, MainLayout],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
